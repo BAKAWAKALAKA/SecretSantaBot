c625d2cdf829e9df6c09eec5babadd3d45f3d75a
katryn36 <katryn36@gmail.com>
2019-12-11 02:09:36 +0300

    add proxy grabber

---
+++ ProxyParser/ProxyServerGrabberFromFile.cs
@@ -0,0 +1,68 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ProxyParser
+{
+    public class ProxyServerGrabberFromFile
+    {
+        private string _base = "http://spys.me/proxy.txt";
+        public string Content { get; set; }
+        public string current_proxy
+        {
+            get
+            {
+                if (_list.Any())
+                {
+                    var res = _list.First();
+                    _list.Remove(res);
+                    return res;
+                }
+                return null;
+            }
+        }
+        private List<string> _list = new List<string>();
+        private Timer _timer;
+
+        public ProxyServerGrabberFromFile()
+        {
+            SetProxyServerFromFile();
+            _timer = new Timer((x) => this.SetProxyServerFromFile());
+            _timer.Change(new TimeSpan(), new TimeSpan(1, 0, 0));
+        }
+
+        public void SetProxyServerFromFile()
+        {
+            var client = new RestClient(_base);
+            var request = new RestRequest(Method.GET);
+            var responce = client.Execute(request);
+            Content = responce.Content;
+            SetProxyServer();
+        }
+
+        public void SetProxyServer()
+        {
+            var regtext = @"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,4} \w*-\w*.*?\n";
+            Regex regexRow = new Regex(regtext);
+            MatchCollection matches = regexRow.Matches(Content);
+
+            foreach (Match match in matches)
+            {
+                var list = match.Value.Split(' ');
+                if (list.Count() > 3)
+                {
+                    if (!list[1].Contains("RU") && (list[1].Contains("-A") || list[1].Contains("-H")))
+                    {
+                        if (list[2].Contains("+"))
+                        {
+                            _list.Add(list[0]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
